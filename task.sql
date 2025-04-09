2dd2e7605460ff0c6f48b4e02739e65e3ca67bf1
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-09 16:41:23 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,2 +1,7 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+CREATE INDEX Name ON Products(Name);
+CREATE UNIQUE INDEX Email ON Customers(Email);
+CREATE INDEX CustomerID ON Orders(CustomerID);
+CREATE INDEX OrderID ON OrderItems(OrderID);
+CREATE INDEX ProductID ON OrderItems(ProductID);
